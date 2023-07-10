963048295b995137f54b2936097e39c8e3863a5e
Subhash Negi <43631257+subhashnegi98@users.noreply.github.com>
2023-07-10 16:28:53 +0000

    about variables

---
+++ variables.js
@@ -0,0 +1,9 @@
+const id = 1234;
+let email = "abc@gmail.com";
+var password = "abc@123";
+city = "New tehri"; //not preferrable
+let state;
+
+console.table([id, email, password, city, state]);
+
+// Refrain from using 'var'; Because of issue in block scope and functional scope.
